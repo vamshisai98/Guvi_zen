7c26143551658ecee4c79be01d9e6a01d8fa4266
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2020-11-06 13:31:06 +0530

    second commit

--- GUVI_18/pets.js
+++ GUVI_18/pets.js
@@ -1,13 +1,40 @@
 var PetsAvailable = /** @class */ (function () {
-    function PetsAvailable(name, id, age) {
-        this.name = name;
-        this.id = id;
-        this.age = age;
+    function PetsAvailable() {
+        this.pets = [];
     }
-    PetsAvailable.prototype.insertPet = function (name, id, age) {
-        return this.name = name, this.id = id, this.age = age;
+    PetsAvailable.prototype.insertPet = function (petinfo) {
+        this.pets.push(petinfo);
+    };
+    PetsAvailable.prototype.listOfPets = function () {
+        return this.pets;
     };
     return PetsAvailable;
 }());
-var petsNew = new PetsAvailable('dog', 1, 2);
-console.log(petsNew.insertPet('cat', 2, 3));
+var RequestPets = /** @class */ (function () {
+    function RequestPets() {
+        this.requestpets = [];
+    }
+    RequestPets.prototype.checkPets = function (reqPets) {
+        this.requestpets.push(reqPets);
+    };
+    RequestPets.prototype.petsQueue = function () {
+        return (this.requestpets.forEach(function (x) {
+            console.log(x.name);
+        }));
+    };
+    return RequestPets;
+}());
+var petsNew = new PetsAvailable();
+petsNew.insertPet({ name: 'Dog', noOfPets: 10, character: 'friendly', id: 1 });
+petsNew.insertPet({ name: 'Cat', noOfPets: 10, character: 'friendly', id: 2 });
+petsNew.insertPet({ name: 'Fish', noOfPets: 10, character: 'friendly', id: 3 });
+petsNew.insertPet({ name: 'Bird', noOfPets: 10, character: 'friendly', id: 4 });
+console.log(petsNew.listOfPets());
+var requestPetAvailablity = new RequestPets();
+requestPetAvailablity.checkPets({ name: 'Dog', reqQuantity: 3, id: 1 });
+requestPetAvailablity.checkPets({ name: 'Cat', reqQuantity: 14, id: 2 });
+requestPetAvailablity.checkPets({ name: 'Bird', reqQuantity: 3, id: 4 });
+requestPetAvailablity.checkPets({ name: 'Cat', reqQuantity: 14, id: 2 });
+requestPetAvailablity.checkPets({ name: 'Fish', reqQuantity: 3, id: 3 });
+requestPetAvailablity.checkPets({ name: 'Cat', reqQuantity: 14, id: 2 });
+console.log(requestPetAvailablity.petsQueue());
--- GUVI_18/pets.ts
+++ GUVI_18/pets.ts
@@ -1,30 +1,63 @@
 type pname = 'Dog'|'Bird'|'Cat'|'Fish'
 
 interface PetsInfo{
-    name:pname
-    age:number
-    quantity:number
-    
+    name?:pname
+    noOfPets?:number
+    character?:string
+    reqQuantity?:number
+    id?:number
+
     
 }
 
 class PetsAvailable{
-    name:pname
-    age:number
-    quantity:number
     pets:Array<PetsInfo>
 
     constructor( ){
       this.pets=[]
     }
 
-    insertPet( ){
-       
+    insertPet(petinfo:PetsInfo ){
+       this.pets.push(petinfo)
     
+    }
+    listOfPets(){
+        return this.pets
     }
 }
 
+class RequestPets {
+    requestpets:Array<PetsInfo>
+    constructor(){
+        this.requestpets=[]
+    }
+    checkPets(reqPets:PetsInfo){
+        this.requestpets.push(reqPets)
+    }
+    petsQueue(){
+        return (this.requestpets.forEach((x)=>{
+            console.log(x.name) 
+        }))
+    }
+  
+  
+}
 
-let petsNew = new PetsAvailable('dog',1,2)
+let petsNew = new PetsAvailable()
 
-console.log(petsNew.insertPet('cat',2,3))
+petsNew.insertPet({name:'Dog',noOfPets:10,character:'friendly',id:1})
+petsNew.insertPet({name:'Cat',noOfPets:10,character:'friendly',id:2})
+petsNew.insertPet({name:'Fish',noOfPets:10,character:'friendly',id:3})
+petsNew.insertPet({name:'Bird',noOfPets:10,character:'friendly',id:4})
+
+console.log(petsNew.listOfPets())
+
+
+let requestPetAvailablity = new RequestPets()
+requestPetAvailablity.checkPets({name:'Dog',reqQuantity:3,id:1})
+requestPetAvailablity.checkPets({name:'Cat',reqQuantity:14,id:2})
+requestPetAvailablity.checkPets({name:'Bird',reqQuantity:3,id:4})
+requestPetAvailablity.checkPets({name:'Cat',reqQuantity:14,id:2})
+requestPetAvailablity.checkPets({name:'Fish',reqQuantity:3,id:3})
+requestPetAvailablity.checkPets({name:'Cat',reqQuantity:14,id:2})
+console.log(requestPetAvailablity.petsQueue())
