017c30701768fbc1df507cc8572d5e4248e3144d
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2020-11-20 10:36:48 +0530

    mongo db query

---
+++ GUVI_23/answes.js
@@ -0,0 +1,9 @@
+
+{ "_id" : ObjectId("5fb68fc37ee26ed0977fae88"), "name" : "Vamshi", "primary_address" : "123 hds nagar Bangalore", "favourite_teacher" : "Ashwini", "age" : "14" }
+{ "_id" : ObjectId("5fb68fc37ee26ed0977fae89"), "name" : "Arun", "primary_address" : "345 adf nagar Chennai", "favourite_teacher" : "Venkat", "age" : "12" }
+{ "_id" : ObjectId("5fb68fc37ee26ed0977fae8a"), "name" : "Vijay", "primary_address" : "123 hds nagar Kolkata", "favourite_teacher" : "Ashwini", "age" : "14" }
+{ "_id" : ObjectId("5fb68fc37ee26ed0977fae8b"), "name" : "Santosh", "primary_address" : "222 afc nagar Chennai", "favourite_teacher" : "Venkat", "age" : "13" }
+{ "_id" : ObjectId("5fb68fc37ee26ed0977fae8c"), "name" : "Naman", "primary_address" : "333 ssdd nagar Bangalore", "favourite_teacher" : "Ashwini", "age" : "12" }
+{ "_id" : ObjectId("5fb68fc37ee26ed0977fae8d"), "name" : "Nisha", "primary_address" : "323 fff nagar Mumbai", "favourite_teacher" : "Venkat", "age" : "13" }
+{ "_id" : ObjectId("5fb68fc37ee26ed0977fae8e"), "name" : "Navya", "primary_address" : "420 sss nagar Bangalore", "favourite_teacher" : "Ashwini", "age" : "13" }
+{ "_id" : ObjectId("5fb68fc37ee26ed0977fae8f"), "name" : "Nihal", "primary_address" : "22 hds nagar Chennai", "favourite_teacher" : "Venkat", "age" : "14" }
