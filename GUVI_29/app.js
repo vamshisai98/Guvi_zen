8f36664abe263faf6009fd8025887a1ccd515449
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2020-12-07 19:32:37 +0530

    connected database and api's created

---
+++ GUVI_29/app.js
@@ -0,0 +1,63 @@
+const express =require("express")
+const mongodb = require("mongodb")
+const cors = require("cors")
+const port = process.env.PORT || 3000
+
+
+const mongoClient = mongodb.MongoClient
+const objectId = mongodb.ObjectId
+
+const app = express()
+require("dotenv").config();
+app.use(express.json())
+app.use(cors())
+
+
+const dbURL = process.env.DB_URL 
+
+
+app.get("/", async(req,res)=>{
+    try {
+        let clientInfo = await mongoClient.connect(dbURL)
+        let db = clientInfo.db("student-mentor-details")
+        let data = await db.collection("portfolio-user").find().toArray()
+        res.status(200).json(data)
+        clientInfo.close()
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+})
+
+
+app.put("/edit",async(req,res)=>{
+    try {
+        let clientInfo = await mongoClient.connect(dbURL)
+        let db = clientInfo.db("student-mentor-details")
+    
+        let data = await db.collection("portfolio-user").updateOne({
+            _id: objectId("5fce321555dae98fdeaf99ad")
+        },
+        {
+            $set:req.body
+        })
+
+        res.status(200).send({
+            message: "user updated"
+        })
+        clientInfo.close()
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+})
+
+
+
+
+
+
+
+app.listen(port, () => console.log("your app runs with port:",port));
--- GUVI_29/frontend/form.js
+++ GUVI_29/frontend/form.js
@@ -1,14 +1,6 @@
 
 
-let addSchool = ()=>{
-   let div = document.createElement('div')
-   div.innerHTML =` <label for="school-name">School</label>
-   <input type="text" class="form-control" id="school-name">
-   <label for="school-cgpa">CGPA</label>
-   <input type="text" class="form-control" id="school-cgpa">`
 
-   document.getElementById('school-details').appendChild(div)
-}
 
 let addWork = ()=>{
     let div = document.createElement('div')
